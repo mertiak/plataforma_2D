5e7464c94e0c2400ef84724ac4512020e2ad22e7
mertiak <jljoorgee@gmail.com>
2016-11-09 18:32:32 +0100

    moneda script y prefab

---
+++ Assets/scripts/generador.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generador : MonoBehaviour {
+	public GameObject moneda;
+	private GameObject moneda_nueva;
+
+	void OnTriggerEnter2D(Collider2D objeto) {
+		if (objeto.tag == "Player" && moneda_nueva == null) {
+			moneda_nueva =(GameObject)Instantiate (moneda, transform.position, transform.rotation);
+		}
+		
+	}
+
+}
---
+++ Assets/scripts/moneda.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moneda : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2 (Random.Range (-100, 100),100));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
