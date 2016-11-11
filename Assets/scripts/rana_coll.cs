4b2405ee89b17fc9d3d4a23ddeb936cdc405f293
mertiak <jljoorgee@gmail.com>
2016-11-11 17:42:33 +0100

    rana_coll

---
+++ Assets/scripts/rana_coll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rana_coll : MonoBehaviour {
+
+	GameObject texto_moneda;
+	control_moneda cm;
+
+	// Use this for initialization
+	void Start () {
+		texto_moneda = GameObject.Find ("texto_moneda");
+		cm = texto_moneda.GetComponent<control_moneda> ();
+	}
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Player") {
+			cm.suma_monedas (-3);
+		}
+	}
+}
