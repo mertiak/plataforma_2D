2b64b603e5ce6783996d4b2ed4bba9e86080a0c0
mertiak <jljoorgee@gmail.com>
2016-11-15 20:33:17 +0100

    banderas_plataformas

---
+++ Assets/scripts/PlataformaMov.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlataformaMov : MonoBehaviour {
+	public GameObject plataforma;
+	public Transform inicio;
+	public Transform fin;
+	public float velocidad = 10f;
+	private Vector3 destino;
+
+	// Use this for initialization
+	void Start () {
+		destino = fin.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		plataforma.transform.position = Vector3.MoveTowards (plataforma.transform.position,
+			destino,
+			velocidad * Time.deltaTime);
+		if (plataforma.transform.position == fin.position) {
+			destino = inicio.position;
+		}
+		if (plataforma.transform.position == inicio.position) {
+			destino = fin.position;
+		}
+	}
+}
--- Assets/scripts/Script_checkpoint.cs
+++ Assets/scripts/Script_checkpoint.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class Script_checkpoint : MonoBehaviour {
 	private Game_control_script gcs;
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator> ();
 		gcs = GameObject.Find ("game_control").GetComponent<Game_control_script> ();
 	
 	}
@@ -13,6 +15,7 @@ public class Script_checkpoint : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D objeto) {
 		if (objeto.tag == ("Player")) {
 			gcs.checkpoint (transform.position);
+			anim.SetBool ("mov", true);
 		}
 	}
 }
