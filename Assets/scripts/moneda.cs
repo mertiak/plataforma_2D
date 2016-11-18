a62147d68140a594117bfeb9bdf8dc27bbfdb5fa
mertiak <jljoorgee@gmail.com>
2016-11-18 17:53:58 +0100

    entrega viernes

--- Assets/scripts/moneda.cs
+++ Assets/scripts/moneda.cs
@@ -8,8 +8,8 @@ public class moneda : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (new Vector2 (Random.Range (-100, 100),100));
+		//rb = GetComponent<Rigidbody2D> ();
+		//rb.AddForce (new Vector2 (Random.Range (-100, 100),100));
 		texto_moneda = GameObject.Find ("texto_moneda");
 		cm = texto_moneda.GetComponent<control_moneda> ();
 	}
