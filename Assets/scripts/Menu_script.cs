b07f327fc8fe938ac2f6a0994228669eaab0ff1e
mertiak <jljoorgee@gmail.com>
2016-11-22 20:43:13 +0100

    pausas

---
+++ Assets/scripts/Menu_script.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Animator))]
+public class Menu_script : MonoBehaviour {
+
+	Animator anim;
+	private bool menu_pausa = false;
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (menu_pausa) {
+				continuar ();
+			} else {
+				pausa ();
+			}
+		}
+	}
+	public void pausa(){
+		menu_pausa = true;
+		Time.timeScale = 0;
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", true);
+		
+	}
+	public void salir(){
+		Application.Quit ();
+		Debug.Log ("Saliendo");
+	}
+
+	public void opciones(){
+		menu_pausa = true;
+		Time.timeScale = 0;
+		anim.SetBool ("opciones", true);
+		anim.SetBool ("pausa", true);
+		
+		
+	}
+	public void continuar(){
+		menu_pausa = false;
+		Time.timeScale = 1;
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", false);
+	}
+}
